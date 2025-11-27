fb94490d91d6a73fab44772141bf9dcd787d6544
OlenaKalitsinska <olena.kalitsinka@gmail.com>
2025-11-27 15:16:54 +0200

    feat: add dotenv support and load BOT_TOKEN from .env

--- bot.py
+++ bot.py
@@ -1,15 +1,20 @@
 import asyncio
+import os
+
 from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
 
 from app.handlers import router
 
+load_dotenv()
 
 async def main():
-    bot = Bot(token='BOT_TOKEN')
+    token = os.getenv("BOT_TOKEN")
+    bot = Bot(token=token)
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
